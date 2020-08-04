ed47675302c6e5a1185e755422c9cd2dc6da35de
Guilherme Dutra <guipereira07@gmail.com>
2020-08-04 16:15:59 -0300

    Add user schema and accounts context

---
+++ lib/banking/schemas/user.ex
@@ -0,0 +1,20 @@
+defmodule Banking.Schemas.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :encrypted_password, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :name, :encrypted_password])
+    |> validate_required([:email, :name, :encrypted_password])
+    |> unique_constraint(:email)
+  end
+end
